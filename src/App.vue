8f9b39438800b3cd19261085cb95b1c083022de2
oushihao <oushihao97@163.com>
2021-12-19 14:38:08 +0800

    style: 修改项目结构

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <h1>{{msg}}</h1>
+        <p @click="addCount">{{count}}</p>
+    </div>
+
+</template>
+
+<script>
+import {ref} from "vue";
+export default {
+    data() {
+        return {
+            msg: "HelloWorld"
+        }
+    },
+    setup() {
+        const count = ref(0);
+
+        const addCount = () => {
+            count.value++;
+        }
+
+        return {
+            count,
+            addCount
+        }
+    }
+}
+</script>
