9c1b6796754f4240340b9a2561e490572725aade
oushihao <oushihao97@163.com>
2021-12-21 14:21:00 +0800

    chore: 配置prettier

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
-    <div class="container">
-        <h1>{{msg}}</h1>
-        <p @click="addCount">{{count}}</p>
-    </div>
-
+	<div class="container">
+		<h1>{{ msg }}</h1>
+		<p @click="addCount">{{ count }}</p>
+	</div>
 </template>
 
 <script>
-import {ref} from "vue";
-export default {
-    data() {
-        return {
-            msg: "HelloWorld"
-        }
-    },
-    setup() {
-        const count = ref(0);
+	import { ref } from 'vue';
+	export default {
+		data() {
+			return {
+				msg: 'HelloWorld'
+			};
+		},
+		setup() {
+			const count = ref(0);
 
-        const addCount = () => {
-            count.value++;
-        }
+			const addCount = () => {
+				count.value++;
+			};
 
-        return {
-            count,
-            addCount
-        }
-    }
-}
+			return {
+				count,
+				addCount
+			};
+		}
+	};
 </script>
