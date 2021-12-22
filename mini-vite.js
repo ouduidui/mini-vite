edb118cbf950b2e9ed5ec35d3333e037ca462e04
oushihao <oushihao97@163.com>
2021-12-22 13:29:12 +0800

    style: 格式化代码

--- mini-vite.js
+++ mini-vite.js
@@ -8,64 +8,64 @@ const koa = require('koa');
 const app = new koa();
 
 app.use(async (ctx) => {
-	// 获取页面地址和参数
-	const {
-		request: { url, query }
-	} = ctx;
+  // 获取页面地址和参数
+  const {
+    request: { url, query }
+  } = ctx;
 
-	// 首页
-	if (url === '/') {
-		ctx.type = 'text/html';
-		let content = fs.readFileSync('index.html', 'utf-8');
-		content = content.replace(
-			'<script ',
-			`
+  // 首页
+  if (url === '/') {
+    ctx.type = 'text/html';
+    let content = fs.readFileSync('index.html', 'utf-8');
+    content = content.replace(
+      '<script ',
+      `
             <script>
                 window.process = {env:{ NODE_ENV:'dev'}}
             </script>
             <script
            `
-		);
-		ctx.body = content;
-	}
+    );
+    ctx.body = content;
+  }
 
-	// js文件
-	else if (url.endsWith('.js')) {
-		// 获取js文件的绝对路径
-		const p = path.resolve(__dirname, url.slice(1));
-		ctx.type = 'application/javascript';
-		const content = fs.readFileSync(p, 'utf-8');
-		// 修改js中的引入文件
-		ctx.body = rewriteImport(content);
-	}
+  // js文件
+  else if (url.endsWith('.js')) {
+    // 获取js文件的绝对路径
+    const p = path.resolve(__dirname, url.slice(1));
+    ctx.type = 'application/javascript';
+    const content = fs.readFileSync(p, 'utf-8');
+    // 修改js中的引入文件
+    ctx.body = rewriteImport(content);
+  }
 
-	// node_modules中的模块文件
-	else if (url.startsWith('/@modules/')) {
-		// 获取模块目录的绝对路径
-		const prefix = path.resolve(__dirname, 'node_modules', url.replace('/@modules/', ''));
-		// 从package.json中获取模块主函数的路径
-		const module = require(prefix + '/package.json').module;
-		const p = path.resolve(prefix, module);
+  // node_modules中的模块文件
+  else if (url.startsWith('/@modules/')) {
+    // 获取模块目录的绝对路径
+    const prefix = path.resolve(__dirname, 'node_modules', url.replace('/@modules/', ''));
+    // 从package.json中获取模块主函数的路径
+    const module = require(prefix + '/package.json').module;
+    const p = path.resolve(prefix, module);
 
-		const ret = fs.readFileSync(p, 'utf-8');
-		ctx.type = 'application/javascript';
-		ctx.body = rewriteImport(ret);
-	}
+    const ret = fs.readFileSync(p, 'utf-8');
+    ctx.type = 'application/javascript';
+    ctx.body = rewriteImport(ret);
+  }
 
-	// vue单文件组件
-	else if (url.includes('.vue')) {
-		// 获取vue文件的绝对路径
-		const p = path.resolve(__dirname, url.split('?')[0].slice(1));
-		// vue单文件解析为AST
-		const { descriptor } = compilerSfc.parse(fs.readFileSync(p, 'utf-8'));
+  // vue单文件组件
+  else if (url.includes('.vue')) {
+    // 获取vue文件的绝对路径
+    const p = path.resolve(__dirname, url.split('?')[0].slice(1));
+    // vue单文件解析为AST
+    const { descriptor } = compilerSfc.parse(fs.readFileSync(p, 'utf-8'));
 
-		if (!query.type) {
-			// 获取script的内容
-			const scriptContent = descriptor.script ? descriptor.script.content : descriptor.scriptSetup.content;
-			// 替换默认导出为一个常量
-			const script = scriptContent.replace('export default ', 'const __script = ');
-			ctx.type = 'application/javascript';
-			ctx.body = `
+    if (!query.type) {
+      // 获取script的内容
+      const scriptContent = descriptor.script ? descriptor.script.content : descriptor.scriptSetup.content;
+      // 替换默认导出为一个常量
+      const script = scriptContent.replace('export default ', 'const __script = ');
+      ctx.type = 'application/javascript';
+      ctx.body = `
             ${rewriteImport(script)}
 
             // 解析template
@@ -73,33 +73,33 @@ app.use(async (ctx) => {
             __script.render = __render
             export default __script
             `;
-		} else if (query.type === 'template') {
-			// 模块内容
-			const template = descriptor.template;
-			// 使用vue提供的编译器将template编译为render函数
-			const render = compilerDom.compile(template.content, { mode: 'module' }).code;
+    } else if (query.type === 'template') {
+      // 模块内容
+      const template = descriptor.template;
+      // 使用vue提供的编译器将template编译为render函数
+      const render = compilerDom.compile(template.content, { mode: 'module' }).code;
 
-			ctx.type = 'application/javascript';
-			ctx.body = rewriteImport(render);
-		}
-	}
+      ctx.type = 'application/javascript';
+      ctx.body = rewriteImport(render);
+    }
+  }
 });
 
 // 将引入的第三方插件指向@modules路径
 function rewriteImport(content) {
-	return content.replace(/ from ['|"]([^'"]+)['|"]/g, (s0, s1) => {
-		if (s1[0] !== '.' && s1[1] !== '/') {
-			return ` from '/@modules/${s1}'`;
-		} else {
-			return s0;
-		}
-	});
+  return content.replace(/ from ['|"]([^'"]+)['|"]/g, (s0, s1) => {
+    if (s1[0] !== '.' && s1[1] !== '/') {
+      return ` from '/@modules/${s1}'`;
+    } else {
+      return s0;
+    }
+  });
 }
 
 // 开启服务
 app.listen(8080, () => {
-	console.log(
-		` App running at:
+  console.log(
+    ` App running at:
   - Local:   http://localhost:8080/`
-	);
+  );
 });
